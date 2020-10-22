2aa2be39f58b459dc78ffe243abd4385bf2005b6
fzhedu <fzhedu@gmail.com>
2020-10-22 18:52:23 +0800

    use one thread to write all channels

--- Sync/ServerWrite/exchange_receiver.cpp
+++ Sync/ServerWrite/exchange_receiver.cpp
@@ -7,6 +7,7 @@
 #include <grpcpp/grpcpp.h>
 #include <grpc/support/log.h>
 #include <thread>
+#include <condition_variable>
 #include "exchange.grpc.pb.h"
 #include "../../exchange.h"
 #include <grpcpp/resource_quota.h>
@@ -35,10 +36,14 @@ ReqChunk* GenChunk(int num) {
     return chk;
 }
 class ExchangeServiceImp final : public ExchangeService::Service {
-public: explicit ExchangeServiceImp(int client_num):client_num_(client_num),receive_chunk_num(0){
+public: explicit ExchangeServiceImp(int client_num):client_num_(client_num),receive_chunk_num(0), connected_clients_(0){
     chunk_ = GenChunk(0);
+        th = thread(&ExchangeServiceImp::SendData, this);
     }
-    Status ExchangeDataRet(ServerContext* context, const Empty* request, ServerWriter<ReqChunk>* writer) override {
+    ~ExchangeServiceImp() {
+        th.join();
+    }
+/*    Status ExchangeDataRet(ServerContext* context, const Empty* request, ServerWriter<ReqChunk>* writer) override {
         while (writer->Write(*chunk_)) {
             receive_chunk_num++;
             if(receive_chunk_num % MOD_LIMIT ==0) {
@@ -46,12 +51,44 @@ public: explicit ExchangeServiceImp(int client_num):client_num_(client_num),rece
             }
         }
         return Status::OK;
+    }*/
+    Status ExchangeDataRet(ServerContext* context, const Empty* request, ServerWriter<ReqChunk>* writer) override {
+        mtx.lock();
+        writers.emplace_back(writer);
+        connected_clients_++;
+        mtx.unlock();
+        if (connected_clients_ >= client_num_) {
+            cv.notify_all();
+        }
+        // block for finish
+        std::unique_lock<std::mutex> lck(mtx);
+        cv_finish.wait(lck);
+        return Status::OK;
+    }
+    void SendData() {
+        std::unique_lock<std::mutex> lck(mtx);
+        cv.wait(lck ,[&] {return connected_clients_ >= client_num_;});
+        while (true) {
+            for(auto i= 0 ;i< connected_clients_;++i) {
+                writers[i]->Write(*chunk_);
+            }
+            receive_chunk_num ++;
+            if(receive_chunk_num % MOD_LIMIT ==0) {
+                cout << "exchange write chunks = "<< receive_chunk_num<<endl;
+            }
+        }
+
     }
 
 private:
+    std::mutex mtx;
+    thread th;
+    std::condition_variable cv, cv_finish;
     ReqChunk* chunk_;
     int client_num_;
     std::atomic_int receive_chunk_num;
+    vector<ServerWriter<ReqChunk>* > writers;
+    atomic_int  connected_clients_;
 };
 void RunServer(string ip, string port, int client_num) {
     std::string server_address(ip+ ":"+port);
@@ -59,13 +96,14 @@ void RunServer(string ip, string port, int client_num) {
     ServerBuilder builder;
 
     // set resource quota
-    grpc::ResourceQuota quota;
-    quota.SetMaxThreads(1);
-    builder.SetResourceQuota(quota);
+//    grpc::ResourceQuota quota;
+//    quota.SetMaxThreads(2);
+//    builder.SetResourceQuota(quota);
 
     builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
     builder.RegisterService(&service);
     std::unique_ptr<Server> server(builder.BuildAndStart());
+
     std::cout << "Server listening on " << server_address << std::endl;
     server->Wait();
 }
--- exchange.h
+++ exchange.h
@@ -5,7 +5,7 @@
 #ifndef EXCHANGE_EXCHANGE_H
 #define EXCHANGE_EXCHANGE_H
 #define LIMIT 1000000000
-#define MOD_LIMIT 100000
+#define MOD_LIMIT 100
 #define CHUNK_NUM 10240
 #define THREAD_NUM 4
 #include <string>
