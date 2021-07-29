8163fef9740b160edb5b65d36dc6ce064fbaee9c
fzhedu <fzhedu@gmail.com>
2021-07-29 16:49:17 +0800

    100 chunks

--- Sync/ServerWrite/exchange_sender.cpp
+++ Sync/ServerWrite/exchange_sender.cpp
@@ -1,11 +1,11 @@
-#include <memory>
-#include <iostream>
-#include <string>
-#include <grpcpp/grpcpp.h>
-#include <grpc/support/log.h>
-#include <thread>
 #include "../../exchange.grpc.pb.h"
 #include "../../exchange.h"
+#include <grpc/support/log.h>
+#include <grpcpp/grpcpp.h>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
 using grpc::Channel;
 using grpc::ClientContext;
 using grpc::ClientReader;
@@ -14,73 +14,77 @@ using grpc::ClientWriter;
 using grpc::Status;
 using namespace exchange;
 using namespace std;
+#define CHUNK_LIMIT 100
 
 class ExchangeClient {
 public:
-    ExchangeClient(std::shared_ptr<Channel> channel, int client_id)
-            : stub_(ExchangeService::NewStub(channel)),client_id_(client_id) {
-        chunk_num_ = 0;
+  ExchangeClient(std::shared_ptr<Channel> channel, int client_id)
+      : stub_(ExchangeService::NewStub(channel)), client_id_(client_id) {
+    chunk_num_ = 0;
+  }
+  void SendData() {
+    ClientContext context;
+    Empty empty;
+    for (auto i = 0; i < CHUNK_LIMIT; ++i) {
+      chunks.push_back(new ReqChunk());
     }
-    void SendData() {
-        ClientContext context;
-        Empty empty;
-        for(auto i=0;i<MOD_LIMIT;++i)
-        {
-          chunks.push_back(new ReqChunk());
-        }
-        //chunk_ = new ReqChunk();
-        uint64_t  recv_size=0;
-        std::unique_ptr<ClientReader<ReqChunk> > reader(stub_->ExchangeDataRet(&context, empty));
-        while (reader->Read(chunks[chunk_num_%MOD_LIMIT])){
-          chunk_ = chunks[chunk_num_%MOD_LIMIT];
-            if (chunk_num_ % MOD_LIMIT ==0 ) {
-              cout<<client_id_ <<" client read chunks = "<< chunk_num_<<"  "<< chunk_->ByteSizeLong() <<endl;
-            }
-          recv_size += chunk_->ByteSizeLong();
-        //  chunk_->Swap(chunks[chunk_num_%MOD_LIMIT]);
-          chunk_num_++;
-        }
-        Status status = reader->Finish();
-        if (status.ok()) {
-            std::cout << "Send data rpc succeeded." << std::endl;
-        } else {
-            std::cout << "send Data rpc failed." << std::endl;
-        }
+    uint64_t recv_size = 0;
+    std::unique_ptr<ClientReader<ReqChunk>> reader(
+        stub_->ExchangeDataRet(&context, empty));
+    while (reader->Read(chunks[chunk_num_ % CHUNK_LIMIT])) {
+      chunk_ = chunks[chunk_num_ % CHUNK_LIMIT];
+      if (chunk_num_ % MOD_LIMIT == 0) {
+        cout << client_id_ << " client read chunks = " << chunk_num_ << "  "
+             << chunk_->ByteSizeLong() << endl;
+      }
+      recv_size += chunk_->ByteSizeLong();
+      //  chunk_->Swap(chunks[chunk_num_%MOD_LIMIT]);
+      chunk_num_++;
     }
+    Status status = reader->Finish();
+    if (status.ok()) {
+      std::cout << "Send data rpc succeeded." << std::endl;
+    } else {
+      std::cout << "send Data rpc failed." << std::endl;
+    }
+  }
 
 private:
-    std::unique_ptr<ExchangeService::Stub> stub_;
-    ReqChunk* chunk_;
-    std::vector<ReqChunk*> chunks;
-    atomic_int chunk_num_;
-    int client_id_;
+  std::unique_ptr<ExchangeService::Stub> stub_;
+  ReqChunk *chunk_;
+  std::vector<ReqChunk *> chunks;
+  atomic_int chunk_num_;
+  int client_id_;
 };
 
+int main(int argc, char **argv) {
+  std::cout << "./sender 'n servers' '1-th ip' '1-th port' ... 'req num'"
+            << std::endl;
+  assert(argc > 2);
+  int client_num = atoi(argv[1]);
+  assert(2 * client_num + 3 == argc);
+  for (int i = 0; i < client_num; ++i) {
+    addr[i].ip = argv[2 * i + 2];
+    addr[i].port = argv[2 * i + 3];
+  }
+  vector<thread> threads;
+  vector<ExchangeClient *> clients;
+  int req_num = atoi(argv[argc - 1]);
+  for (int i = 0; i < client_num; ++i) {
+    grpc::ChannelArguments channelArgs;
+    channelArgs.SetMaxReceiveMessageSize(MSG_SIZE);
+    channelArgs.SetMaxSendMessageSize(MSG_SIZE);
+    ExchangeClient *new_client =
+        new ExchangeClient(grpc::CreateCustomChannel(
+                               addr[i].ip + ":" + addr[i].port,
+                               grpc::InsecureChannelCredentials(), channelArgs),
+                           i);
+    clients.emplace_back(new_client);
+    threads.emplace_back(thread(&ExchangeClient::SendData, new_client));
+  }
 
-int main(int argc, char** argv) {
-    std::cout<<"./sender 'n servers' '1-th ip' '1-th port' ... 'req num'"<<std::endl;
-    assert(argc>2);
-    int client_num=atoi(argv[1]);
-    assert(2*client_num+3==argc);
-    for(int i=0; i<client_num; ++i) {
-        addr[i].ip = argv[2*i+2];
-        addr[i].port = argv[2*i+3];
-    }
-    vector<thread>threads;
-    vector<ExchangeClient*>clients;
-    int req_num=atoi(argv[argc-1]);
-    for (int i=0;i< client_num;++i) {
-        grpc::ChannelArguments  channelArgs;
-        channelArgs.SetMaxReceiveMessageSize(MSG_SIZE);
-        channelArgs.SetMaxSendMessageSize(MSG_SIZE);
-        ExchangeClient* new_client =new ExchangeClient(grpc::CreateCustomChannel(addr[i].ip+":"+addr[i].port,
-                                              grpc::InsecureChannelCredentials(),channelArgs),i);
-        clients.emplace_back(new_client);
-        threads.emplace_back(thread(&ExchangeClient::SendData,new_client));
-    }
-
-    for(int i=0;i< client_num;++i) {
-        threads[i].join();
-    }
-    return 0;
+  for (int i = 0; i < client_num; ++i) {
+    threads[i].join();
+  }
+  return 0;
 }
